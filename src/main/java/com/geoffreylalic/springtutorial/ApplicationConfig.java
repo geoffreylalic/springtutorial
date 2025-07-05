83f4bc0003486bae2f0f4530047ce88ccbf2c00a
Geoffrey <geoffrey0151@gmail.com>
2025-07-05 21:58:55 +0200

    management of profiles

--- src/main/java/com/geoffreylalic/springtutorial/ApplicationConfig.java
+++ src/main/java/com/geoffreylalic/springtutorial/ApplicationConfig.java
@@ -4,19 +4,22 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean()
     @Qualifier("bean1")
+    @Profile("dev")
     public MyFirstClass myFirstClass() {
-        return new MyFirstClass("Steak");
+        return new MyFirstClass("dev");
     }
 
     @Bean()
     @Qualifier("bean2")
+    @Profile("test")
     public MyFirstClass mySecondClass() {
-        return new MyFirstClass("Thon");
+        return new MyFirstClass("Test");
     }
 }
