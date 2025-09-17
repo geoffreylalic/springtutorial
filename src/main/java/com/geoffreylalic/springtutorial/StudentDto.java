f81fc5f9fbd80e878295ad5fea96bc59cf8e5160
Geoffrey <geoffrey0151@gmail.com>
2025-09-17 15:09:13 +0200

    added student dto

---
+++ src/main/java/com/geoffreylalic/springtutorial/StudentDto.java
@@ -0,0 +1,8 @@
+package com.geoffreylalic.springtutorial;
+
+public record StudentDto(String firstName,
+                         String lastName,
+                         String email,
+                         Integer schoolId) {
+
+}
