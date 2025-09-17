feb757ed2225c881718822673ad9223491b78e65
Geoffrey <geoffrey0151@gmail.com>
2025-09-17 15:47:38 +0200

    used studentdto to create student

--- src/main/java/com/geoffreylalic/springtutorial/StudentController.java
+++ src/main/java/com/geoffreylalic/springtutorial/StudentController.java
@@ -9,43 +9,56 @@ import java.util.List;
 @RestController()
 public class StudentController {
 
-    private final StudentRepository repository;
+    private final StudentRepository studentRepository;
+    private final SchoolRepository schoolRepository;
 
 
-    public StudentController(StudentRepository repository) {
-        this.repository = repository;
+    public StudentController(StudentRepository studentRepository, SchoolRepository schoolRepository) {
+        this.studentRepository = studentRepository;
+        this.schoolRepository = schoolRepository;
     }
 
     @PostMapping("/students")
-    public ResponseEntity<?> createStudent(@RequestBody Student student) {
-        Long nbStudents = repository.countStudentsByEmail(student.getEmail());
+    public ResponseEntity<?> createStudent(@RequestBody StudentDto studentDto) {
+        School school = new School();
+        school.setId(studentDto.schoolId());
+
+        Student student = new Student();
+        student.setFirstName(studentDto.firstName());
+        student.setLastName(studentDto.lastName());
+        student.setEmail(studentDto.email());
+        student.setSchool(school);
+
+
+
+        Long nbStudents = studentRepository.countStudentsByEmail(student.getEmail());
         if (nbStudents > 0) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student already exists.");
         }
-        repository.save(student);
+        studentRepository.save(student);
         return new ResponseEntity<Student>(student, HttpStatus.OK);
     }
 
 
     @GetMapping("/students/search/{student-name}")
     public List<Student> getStudentsByName(@PathVariable("student-name") String studentName) {
-        return repository.findAllByFirstNameContaining(studentName);
+        return studentRepository.findAllByFirstNameContaining(studentName);
     }
 
     @GetMapping("/students/{student-id}")
     public Student postStudent(@PathVariable("student-id") Integer studentId) {
-        return repository.findById(studentId).orElse(null);
+        return studentRepository.findById(studentId).orElse(null);
     }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return repository.findAll();
+        return studentRepository.findAll();
     }
 
     @DeleteMapping("/students/{student-id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteStudent(@PathVariable("student-id") Integer studentId) {
-        repository.deleteById(studentId);
+        studentRepository.deleteById(studentId);
     }
 
 
