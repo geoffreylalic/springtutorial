e716d421cfd201d2e222ce4e7a7e0aaf1a553f39
Geoffrey <geoffrey0151@gmail.com>
2025-09-19 10:54:33 +0200

    refactored business logic of student to service

--- src/main/java/com/geoffreylalic/springtutorial/StudentController.java
+++ src/main/java/com/geoffreylalic/springtutorial/StudentController.java
@@ -1,85 +1,44 @@
 package com.geoffreylalic.springtutorial;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController()
 public class StudentController {
 
-    private final StudentRepository studentRepository;
-    private final SchoolRepository schoolRepository;
+    StudentService studentService;
 
 
-    public StudentController(StudentRepository studentRepository, SchoolRepository schoolRepository) {
-        this.studentRepository = studentRepository;
-        this.schoolRepository = schoolRepository;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
     @PostMapping("/students")
-    public ResponseEntity<?> createStudent(@RequestBody StudentDto studentDto) {
-        Student student = toStudent(studentDto);
-        Long nbStudents = studentRepository.countStudentsByEmail(student.getEmail());
-        if (nbStudents > 0) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student already exists.");
-        }
-        studentRepository.save(student);
-        StudentResponseDto studentResponseDto = new StudentResponseDto(student.getId(), student.getFirstName(), student.getLastName(), student.getEmail());
-        return new ResponseEntity<StudentResponseDto>(studentResponseDto, HttpStatus.OK);
+    public StudentResponseDto createStudent(@RequestBody StudentDto studentDto) {
+        return studentService.createStudent(studentDto);
     }
-
-    private Student toStudent(StudentDto studentDto) {
-        School school = new School();
-        school.setId(studentDto.schoolId());
-
-        Student student = new Student();
-        student.setFirstName(studentDto.firstName());
-        student.setLastName(studentDto.lastName());
-        student.setEmail(studentDto.email());
-        student.setSchool(school);
-
-        return student;
-    }
-
 
     @GetMapping("/students/search/{student-name}")
-    public List<StudentResponseDto> getStudentsByName(@PathVariable("student-name") String studentName) {
-        List<Student> students = studentRepository.findAllByFirstNameContaining(studentName);
-        List<StudentResponseDto> studentResponseDtos = new ArrayList<>();
-        for (Student student : students) {
-            studentResponseDtos.add(new StudentResponseDto(student.getId(), student.getFirstName(), student.getLastName(), student.getEmail()));
-        }
-        return studentResponseDtos;
+    public List<StudentResponseDto> listStudentByFirstName(@PathVariable("student-name") String studentName) {
+        return studentService.listStudentByFirstName(studentName);
     }
 
     @GetMapping("/students/{student-id}")
-    public StudentResponseDto postStudent(@PathVariable("student-id") Integer studentId) {
-        Student student = studentRepository.findById(studentId).orElse(null);
-        if (student != null) {
-            return new StudentResponseDto(student.getId(), student.getFirstName(), student.getLastName(), student.getEmail());
-        }
-        return null;
+    public StudentResponseDto getStudentById(@PathVariable("student-id") Integer studentId) {
+        return studentService.getStudentById(studentId);
     }
 
     @GetMapping("/students")
-    public List<StudentResponseDto> getStudents() {
-        List<Student> students = studentRepository.findAll();
-        List<StudentResponseDto> studentResponseDtos = new ArrayList<>();
-
-        for (Student student : students) {
-            studentResponseDtos.add(new StudentResponseDto(student.getId(), student.getFirstName(), student.getFirstName(), student.getEmail()));
-        }
-        return studentResponseDtos;
-
+    public List<StudentResponseDto> listStudents() {
+        return studentService.listStudents();
     }
 
     @DeleteMapping("/students/{student-id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteStudent(@PathVariable("student-id") Integer studentId) {
-        studentRepository.deleteById(studentId);
+        studentService.deleteStudent(studentId);
     }
 
 
