26ed1fba709327f4841e5c1095bef16c3fc06384
Geoffrey <geoffrey0151@gmail.com>
2025-09-19 15:45:55 +0200

    refactored school controller to school service

--- src/main/java/com/geoffreylalic/springtutorial/SchoolController.java
+++ src/main/java/com/geoffreylalic/springtutorial/SchoolController.java
@@ -5,35 +5,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class SchoolController {
-    private final SchoolRepository schoolRepository;
-    private final SchoolMapper schoolMapper;
 
-    public SchoolController(SchoolRepository repository, SchoolMapper schoolMapper) {
-        this.schoolRepository = repository;
-        this.schoolMapper = schoolMapper;
+    SchoolService schoolService;
+
+    public SchoolController(SchoolService schoolService) {
+        this.schoolService = schoolService;
     }
 
     @PostMapping("/schools")
     public School createSchool(@RequestBody SchoolDto schoolDto) {
-        School school = toSchool(schoolDto);
-        return schoolRepository.save(school);
+        return schoolService.createSchool(schoolDto);
     }
 
-    public School toSchool(SchoolDto schoolDto) {
-        School school = new School();
-        school.setName(schoolDto.name());
-        return school;
-    }
 
     @GetMapping("/schools")
     public List<SchoolResponseDto> listSchools() {
-        List<School> schools = schoolRepository.findAll();
-        return schoolMapper.toResponseDtos(schools);
+        return schoolService.listSchools();
     }
 
 
