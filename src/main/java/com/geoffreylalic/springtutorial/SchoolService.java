6f8c8a13802c75e000ff056a82decf5750bc25f8
Geoffrey <geoffrey0151@gmail.com>
2025-09-19 15:45:17 +0200

    added school service

---
+++ src/main/java/com/geoffreylalic/springtutorial/SchoolService.java
@@ -0,0 +1,28 @@
+package com.geoffreylalic.springtutorial;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SchoolService {
+
+    private final SchoolRepository schoolRepository;
+    private final SchoolMapper schoolMapper;
+
+    public SchoolService(SchoolRepository schoolRepository, SchoolMapper schoolMapper) {
+        this.schoolRepository = schoolRepository;
+        this.schoolMapper = schoolMapper;
+    }
+
+    public School createSchool(SchoolDto schoolDto) {
+        return schoolRepository.save(schoolMapper.toSchool(schoolDto));
+    }
+
+    public List<SchoolResponseDto> listSchools(){
+        List<School> schools = schoolRepository.findAll();
+        return schoolMapper.toResponseDtos(schools);
+    }
+
+
+}
