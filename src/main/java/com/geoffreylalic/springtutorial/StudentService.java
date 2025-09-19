91ff50f1001c3e4247bb886974bf48acfce9e75e
Geoffrey <geoffrey0151@gmail.com>
2025-09-19 10:53:18 +0200

    added student service

---
+++ src/main/java/com/geoffreylalic/springtutorial/StudentService.java
@@ -0,0 +1,50 @@
+package com.geoffreylalic.springtutorial;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final StudentMapper studentMapper;
+
+    public StudentService(StudentRepository studentRepository, StudentMapper studentMapper) {
+        this.studentRepository = studentRepository;
+        this.studentMapper = studentMapper;
+    }
+
+    public StudentResponseDto createStudent(StudentDto studentDto) {
+        Student student = studentMapper.toStudent(studentDto);
+        Long nbStudents = studentRepository.countStudentsByEmail(student.getEmail());
+        if (nbStudents > 0) {
+            return null;
+        }
+        studentRepository.save(student);
+        return studentMapper.toResponseDto(student);
+    }
+
+    public List<StudentResponseDto> listStudentByFirstName(String studentName) {
+        List<Student> students = studentRepository.findAllByFirstNameContaining(studentName);
+        return studentMapper.toResponseDtos(students);
+    }
+
+    public StudentResponseDto getStudentById(Integer studentId) {
+        Student student = studentRepository.findById(studentId).orElse(null);
+        if (student != null) {
+            return studentMapper.toResponseDto(student);
+        }
+        return null;
+    }
+
+    public List<StudentResponseDto> listStudents() {
+        List<Student> students = studentRepository.findAll();
+        return studentMapper.toResponseDtos(students);
+    }
+
+    public void deleteStudent(Integer studentId) {
+        studentRepository.deleteById(studentId);
+    }
+
+}
